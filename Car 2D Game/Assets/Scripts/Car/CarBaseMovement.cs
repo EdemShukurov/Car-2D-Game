437bc8967384c67c10f2fcee85acfcf3e41fe5bc
EdemShukurov <43962752+EdemShukurov@users.noreply.github.com>
2020-03-03 13:30:54 +0500

    Limit car angle

--- Car 2D Game/Assets/Scripts/Car/CarBaseMovement.cs
+++ Car 2D Game/Assets/Scripts/Car/CarBaseMovement.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(WheelJoint2D))]
 public class CarBaseMovement : MonoBehaviour, IVehicleMovable
 {
-    public Rigidbody2D rigidBody2D;
-    
+    public const float GRAVITY = 9.81f;  
+
+    [Header("Movement properies")]
+    [SerializeField] private float brakeForce = 600f;
+    [SerializeField] private float maxSpeed = -1500f;
+    [SerializeField] private float jumpForce = 9f;
+    [SerializeField] private float maxBackSpeed = 600f;
+    [SerializeField] private float acceleration = 300f;
+    [SerializeField] private float deacceleration = -100f;
+
+    [Header("Limit angle")]
+    [SerializeField] private float _minimumZ = -20F;
+    [SerializeField] private float _maximumZ = 20F;
+
     private int _screenWidth;
+
+    protected Rigidbody2D _rigidBody2D;
+
     private float _movementInput;
     private float _deltaMovement;
 
-    private JointMotor2D _frontWheelMotor, _backWheelMotor;
+    private float _physicValue;
+
+    private float _angleCar;
+
+    private JointMotor2D  _wheelMotor;
     private WheelJoint2D[] wheelJoints;
 
     private void Start()
@@ -19,19 +39,21 @@ public class CarBaseMovement : MonoBehaviour, IVehicleMovable
         // set screen width
         _screenWidth = Screen.width;
 
-        wheelJoints = (WheelJoint2D[])rigidBody2D.GetComponents(typeof(WheelJoint2D));
+        wheelJoints = (WheelJoint2D[])_rigidBody2D.GetComponents(typeof(WheelJoint2D));
 
-        if (rigidBody2D == null || wheelJoints == null)
+        if (_rigidBody2D == null || wheelJoints == null)
         {
             throw new ArgumentNullException();
         }
 
-        _frontWheelMotor = wheelJoints[0].motor;
-        _backWheelMotor = wheelJoints[1].motor;
+        _wheelMotor = wheelJoints[0].motor;
+        
     }
 
     private void Update()
     {
+        LimitAngleCar();
+
         _movementInput = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetMouseButton(0))
@@ -41,6 +63,36 @@ public class CarBaseMovement : MonoBehaviour, IVehicleMovable
         }
 
         UpdateVelocity();
+    }
+
+    /// <summary>
+    /// Limit car angle in order to avoid car flip
+    /// </summary>
+    private void LimitAngleCar()
+    {
+        _angleCar = ClampAngle(transform.eulerAngles.z, _minimumZ, _maximumZ);
+        transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, _angleCar);
+    }
+
+    /// <summary>
+    /// Clamp car angle
+    /// </summary>
+    /// <param name="angle"></param>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns>permissible car angle</returns>
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+
+        if (angle > 360F)
+            angle -= 360F;
+
+        if (angle >= 180f)
+            angle -= 360f;
+
+        return Mathf.Clamp(angle, min, max);
     }
 
     /// <summary>
@@ -68,7 +120,7 @@ public class CarBaseMovement : MonoBehaviour, IVehicleMovable
         switch (_movementInput)
         {
             case -1f:
-                if (_backWheelMotor.motorSpeed >= 0)
+                if (_wheelMotor.motorSpeed >= 0)
                     ReverseGear();
                 else
                     Brake();
@@ -79,7 +131,7 @@ public class CarBaseMovement : MonoBehaviour, IVehicleMovable
                 break;
 
             case 1f:
-                if (_backWheelMotor.motorSpeed <= 0)
+                if (_wheelMotor.motorSpeed <= 0)
                     Gas();
                 else
                     Brake();
@@ -89,9 +141,9 @@ public class CarBaseMovement : MonoBehaviour, IVehicleMovable
         if (Input.GetKey(KeyCode.Space))
             Brake();
 
-
+        //set wheels motor speed 
         foreach (var item in wheelJoints)
-            item.motor = _backWheelMotor;
+            item.motor = _wheelMotor;
     }
 
     public void Brake()
@@ -101,7 +153,7 @@ public class CarBaseMovement : MonoBehaviour, IVehicleMovable
 
     public void DisableMovement()
     {
-        throw new System.NotImplementedException();
+
     }
 
     public void Gas()
