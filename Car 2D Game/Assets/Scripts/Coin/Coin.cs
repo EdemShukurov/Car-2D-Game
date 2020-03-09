7137c4599e6eb6a07f6729eb13f2de4146d8e179
EdemShukurov <43962752+EdemShukurov@users.noreply.github.com>
2020-03-09 17:15:35 +0500

    Add coin collector

---
+++ Car 2D Game/Assets/Scripts/Coin/Coin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<Wallet>(out Wallet wallet))
+        {
+            wallet?.AddCoin();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Car 2D Game/Assets/Scripts/Coin/Wallet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using TMPro;
+
+public class Wallet : MonoBehaviour
+{
+    public event Action Changed;
+
+    public TextMeshProUGUI text;
+
+    private int _amount;
+
+    public void AddCoin()
+    {
+        _amount++;
+        text.text = string.Format("X {0}", _amount);
+        Changed?.Invoke();
+    }
+}
+
+
